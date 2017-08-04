fd74cd045700268efab976da47f0f1baa4d0783e
Sagar Shelar <shaggy.shelar@gmail.com>
2017-08-04 06:00:23 +0530

    Added Web project

---
+++ ESPL.BRE.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ESPL.BRE.Web.MvcApplication" Language="C#" %>
---
+++ ESPL.BRE.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ESPL.BRE.Web.Startup))]
+namespace ESPL.BRE.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
