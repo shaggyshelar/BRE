e40a4d81184e56697deedf7ba8e8995c84a198fb
Sagar Shelar <shaggy.shelar@gmail.com>
2017-08-07 15:19:37 +0530

    Added new Demo MVC project

---
+++ ESPL.Rule.Demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ESPL.Rule.Demo.Startup))]
+namespace ESPL.Rule.Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
